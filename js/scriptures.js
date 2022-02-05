c4a056e36f6e70ef77a5894c86e3ab6905a65623
avbaker1998 <70988383+avbaker1998@users.noreply.github.com>
2022-02-04 20:05:07 -0700

    Changed Hashes

--- js/scriptures.js
+++ js/scriptures.js
@@ -1,7 +1,26 @@
+// FILE: scriptures.js
+// AUTHOR: Alle Baker
+// DATE: Winter 2022
+
+/* property
+    books, classKey, content, forEach, hash, href, id, init, location, log,
+    maxBookId, minBookId, onHashChanged, onerror, onload, open, parse, push,
+    response, send, status*/
+
+
 const Scriptures = (function () {
     "use strict";
 
     //Constants
+    const BOTTOM_PADDING = "<br /><br />";
+    const CLASS_BOOKS = "books";
+    const CLASS_VOLUME = "volume";
+    const DIV_SCRIPTURES_NAVIGATOR = "scripnav";
+    const DIV_SCRIPTURES = "scriptures";
+    const REQUEST_GET = "GET";
+    const REQUEST_STATUS_OK = 200;
+    const REQUEST_STATUS_ERROR = 400;
+    const TAG_HEADERS = "h5";
     const URL_BASE = "https://scriptures.byu.edu/";
     const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`;
     const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`;
@@ -14,14 +33,25 @@ const Scriptures = (function () {
     let init;
     let ajax;
     let cacheBooks;
+    let onHashChanged;
+    let htmlAnchor;
+    let htmlDiv;
+    let htmlElement;
+    let htmlLink;
+    let htmlHashLink;
+    let navigateHome;
+    let navigateBook;
+    let navigateChapter;
+    let bookChapterValid;
 
     //Private Methods
     ajax = function (url, successCallback, failureCallback) {
         let request = new XMLHttpRequest();
-        request.open('GET', url, true);
+
+        request.open(REQUEST_GET, url, true);
 
         request.onload = function () {
-            if (request.status >= 200 && request.status < 400) {
+            if (request.status >= REQUEST_STATUS_OK && request.status < REQUEST_STATUS_ERROR) {
                 // Success!
                 let data = JSON.parse(request.response);
 
@@ -93,9 +123,129 @@ const Scriptures = (function () {
         );
     }
 
+    bookChapterValid = function (bookId, chapter) {
+        let book = books[bookId];
+
+        if (book === undefined || chapter < 0 || chapter > book.numChapters) {
+            return false;
+        }
+
+        if (chapter === 0 && book.numChapters > 0) {
+            return false;
+        }
+
+        return true;
+    };
+
+    navigateBook = function (bookId) {
+        console.log("navigateBook " + bookId);
+    };
+    navigateChapter = function (bookId, chapter) {
+        console.log("navigateChapter " + bookId + ", " + chapter);
+    };
+
+    navigateHome = function (volumeId) {
+        document.getElementById(DIV_SCRIPTURES).innerHTML = 
+        "<div>Old Testament</div>" +
+        "<div>New Testament</div>" +
+        "<div>Book of Mormon</div>" +
+        "<div>Doctrine and Covenants</div>" +
+        "<div>Pearl of Great Price</div>" + volumeId;
+    };
+
+    onHashChanged = function () {
+        let ids = [];
+
+        if (location.hash !== "" && location.hash.length > 1) {
+            ids = location.hash.slice(1).split(":");
+        }
+
+        if (ids.length <= 0) {
+            navigateHome();
+        } else if (ids.length === 1) {
+            let volumeId = Number(ids[0]);
+
+            if (volumeId < volumes[0].id || volumeId > volumes.slice(-1)[0].id) {
+                navigateHome();
+            } else {
+                navigateHome(volumeId);
+            }
+        } else {
+            let bookId = Number(ids[1]);
+
+            if (books[bookId === undefined]) {
+                navigateHome();
+            } else {
+                if (ids.length === 2) {
+                    navigateBook(bookId)
+                } else {
+                    let chapter = Number(ids[2]);
+                    if (bookChapterValid(bookId, chapter)) {
+                        navigateChapter(bookId, chapter);
+                    } else {
+                        navigateHome();
+                    }
+                    
+                }
+            }
+        }
+        //console.log(window.location.hash);
+    };
+
+    //idk where he wanted this put in so im putting it in here
+    htmlAnchor = function (volume) {
+        return `<a name="v${volume.id}" />`;
+    };
+
+    htmlDiv = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let idString = "";
+
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+        if (parameters.id !== undefined) {
+            idString = ` id="${parameters.id}"`;
+        }
+        return `<div${idString}${classString}>${contentString0}</div>`;
+    };
+
+    htmlElement = function (tagName, content) {
+        return `<${tagName}>${content}</${tagName}>`;
+    };
+
+    htmlLink = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let hrefString = "";
+        let idString = "";
+
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+        if (parameters.href !== undefined) {
+            hrefString = ` href="${parameters.href}"`;
+        }
+        if (parameters.id !== undefined) {
+            idString = ` id="${parameters.id}"`;
+        }
+        return `<a${idString}${classString}${hrefString}>${contentString0}</a>`;
+    };
+
+    htmlHashLink = function (hashArguments, content) {
+        return `<a href="javascript:void(0)" onclick="changeHash(${hashArguments})">${content}</a>`;
+    };
     //Public API
 
     return {
-        init: init
+        init: init,
+        onHashChanged
     };
-})();
+})(); //window.location.hash.slice(1).split(":")
