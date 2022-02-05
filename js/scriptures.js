4c47c038ae437fd915e0dbfff40a0b846aacabcb
avbaker1998 <70988383+avbaker1998@users.noreply.github.com>
2022-02-05 13:06:51 -0700

    Part 13

--- js/scriptures.js
+++ js/scriptures.js
@@ -19,20 +19,29 @@ const Scriptures = (function () {
     const CLASS_BUTTON = "btn";
     const DIV_SCRIPTURES_NAVIGATOR = "scripnav";
     const DIV_SCRIPTURES = "scriptures";
+    const INDEX_PLACENAME = 2;
+    const INDEX_FLAG = 11;
+    const INDEX_LATITUDE = 3;
+    const INDEX_LONGITUDE = 4;
+    const LAT_LON_PARSER = /\((.*),'(.*)', (.*), (.*), (.*), (.*), (.*), (.*), (.*), (.*), '(.*)'\)/;
     const REQUEST_GET = "GET";
     const REQUEST_STATUS_OK = 200;
     const REQUEST_STATUS_ERROR = 400;
     const TAG_HEADERS = "h5";
     const URL_BASE = "https://scriptures.byu.edu/";
     const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`;
+    const URL_SCRIPTURES = `${URL_BASE}mapscrip/mapgetscrip.php`;
     const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`;
 
     //Private Variables
     let books;
+    let gmMarkers = [];
     let volumes;
+
 
     //Private Method Declarations
     
+    let addMarker;
     let ajax;
     let bookChapterValid;
     let booksGrid;
@@ -40,6 +49,10 @@ const Scriptures = (function () {
     let cacheBooks;
     let chaptersGrid;
     let chaptersGridContent;
+    let clearMarkers;
+    let encodedScripturedUrlParameters;
+    let getScripturesCallback;
+    let getScripturesFailure;
     let htmlAnchor;
     let htmlDiv;
     let htmlElement;
@@ -48,14 +61,29 @@ const Scriptures = (function () {
     let init;
     let navigateBook;
     let navigateChapter;
+    let nextChapter;
     let navigateHome;
     let onHashChanged;
+    let previousChapter;
+    let setupMarkers;
+    let titleForBookChapter;
     let volumesGridContent;
   
 
 
     //Private Methods
-    ajax = function (url, successCallback, failureCallback) {
+
+    addMarker = function (placename, latitude, longitude) {
+        //NEEDSWORD check to see if we already have this let/lon in the gmMarkers array
+        let marker = new google.maps.Marker({
+            position: {lat: Number(latitude), lng: Number(longitude)},
+            map,
+            title: placename,
+            animation: google.maps.Animation.DROP
+        })
+        gmMarkers.push(marker);
+    }
+    ajax = function (url, successCallback, failureCallback, skipJsonParse) {
         let request = new XMLHttpRequest();
 
         request.open(REQUEST_GET, url, true);
@@ -63,7 +91,7 @@ const Scriptures = (function () {
         request.onload = function () {
             if (request.status >= REQUEST_STATUS_OK && request.status < REQUEST_STATUS_ERROR) {
                 // Success!
-                let data = JSON.parse(request.response);
+                let data = (skipJsonParse ? request.response : JSON.parse(request.response));
 
                 if (typeof successCallback === "function") {
                     successCallback(data);
@@ -159,6 +187,37 @@ const Scriptures = (function () {
         return gridContent;
     };
 
+    clearMarkers = function () {
+        gmMarkers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+
+        gmMarkers = [];
+    }
+
+    encodedScripturedUrlParameters = function (bookId, chapter, verses, isJst) {
+        if (bookId !== undefined && chapter !== undefined) {
+            let options = "";
+
+            if (verses !== undefined) {
+                options += verses;
+            }
+            if (isJst !== undefined) {
+                options += "&jst=JST";
+            }
+
+            return `${URL_SCRIPTURES}?book=${bookId}&chap=${chapter}&verses${options}`;
+        }
+    };
+
+    getScripturesCallback = function () {
+        document.getElementById(DIV_SCRIPTURES).innerHTML = chapterHtml;
+        setupMarkers();
+    };
+
+    getScripturesFailure = function () {
+        document.getElementById(DIV_SCRIPTURES).innerHTML = "Unable to retrieve chapter contents."
+    };
     //idk where he wanted this put in so im putting it in here
     htmlAnchor = function (volume) {
         return `<a name="v${volume.id}" />`;
@@ -264,7 +323,8 @@ const Scriptures = (function () {
         //console.log("navigateBook " + bookId);
     };
     navigateChapter = function (bookId, chapter) {
-        console.log("navigateChapter " + bookId + ", " + chapter);
+        ajax(encodedScripturedUrlParameters(bookId, chapter), getScripturesCallback, getScripturesFailure, true);
+        //console.log("navigateChapter " + bookId + ", " + chapter);
     };
 
     navigateHome = function (volumeId) {
@@ -272,6 +332,33 @@ const Scriptures = (function () {
             id: DIV_SCRIPTURES_NAVIGATOR,
             content: volumesGridContent(volumeId)
         });
+    };
+
+    nextChapter = function (book, chapter) {
+
+        if (book !== undefined) {
+            if (chapter < book.numChapters) {
+                return [
+                    bookId,
+                    chapter + 1,
+                    titleForBookChapter(book, chapter + 1)
+                ];
+            }
+            let nextBook = books[bookId + 1];
+
+            if (nextBook !== undefined) {
+                let nextChapterValue = 0;
+                if (nextBook.numChapters > 0) {
+                    nextChapterValue = 1;
+                }
+
+                return[
+                    nextBook.id,
+                    nextChapterValue,
+                    titleForBookChapter(nextBook, nextChapterValue)
+                ];
+            }
+        }
     };
 
     onHashChanged = function () {
@@ -313,6 +400,45 @@ const Scriptures = (function () {
         //console.log(window.location.hash);
     };
 
+    previousChapter = function (bookId, chapter) {
+        //NEEDSWORK: figure this out
+    }
+
+    setupMarkers = function () {
+        if (gmMarkers.length > 0) {
+            clearMarkers();
+        }
+
+        document.querySelectorAll("a[onclick^=\"showLocation(\"]").forEach(function (element) {
+            let matches = LAT_LON_PARSER.exec(element.getAttribute("onclick"));
+
+            if (matches) {
+                let placename = INDEX_PLACENAME;
+                let latitude = INDEX_LATITUDE;
+                let longitude = INDEX_LONGITUDE;
+                let flag = INDEX_FLAG;
+
+                if (flag !== "") {
+                    placename = `${placename} ${flag}`;
+                }
+
+                addMarker(placename, latitude, longitude)
+            }
+        })
+        //matches = /\((.*),'(.*)', (.*), (.*), (.*), (.*), (.*), (.*), (.*), (.*), '(.*)'\)/.exec("showLocation(108, 'Assyria',36.359410,43.152887,33.515336,44.551217,0.000000,0.000000,1202300.000000,0.000000,'>')")
+    };
+
+    titleForBookChapter = function (bookId, chapter) {
+        let book = books(bookId);
+
+        if (book !== undefined) {
+            if (chapter > 0) {
+                return `${book.tocName} ${chapter}`;
+            }
+            return book.tocName;
+        }
+    };
+
     volumesGridContent = function (volumeId) {
         let gridContent = "";
 
@@ -324,7 +450,8 @@ const Scriptures = (function () {
                });
                gridContent += booksGrid(volume);
             }
-        })
+        });
+        return gridContent + BOTTOM_PADDING;
     };
     //Public API
 
@@ -333,3 +460,8 @@ const Scriptures = (function () {
         onHashChanged
     };
 })(); 
+
+//matches = /\((.*),'(.*)', (.*), (.*), (.*), (.*), (.*), (.*), (.*), (.*), '(.*)'\)/.exec("showLocation(108, 'Assyria',36.359410,
+//43.152887,33.515336,44.551217,0.000000,0.000000,1202300.000000,0.000000,'>')")
+
+//API Key: AIzaSyDH1PQNMRrEIBgwpfn5beNKnrIm1CO-T2I
